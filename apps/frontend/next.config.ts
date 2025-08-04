d7f5d0dfbcda0615e3dcaf8429189b60458af40c
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-08-04 22:08:02 +0200

    Update next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false
+	/* config options here */
+	devIndicators: false,
+	allowedDevOrigins: ["https://finura-production.up.railway.app"],
 };
 
 export default nextConfig;
